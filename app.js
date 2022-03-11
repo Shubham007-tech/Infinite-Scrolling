60424815d82a4cc2783adea6c25f512ae409e0a8
Shubham007-tech <91582453+Shubham007-tech@users.noreply.github.com>
2022-03-11 19:41:47 +0530

    done infinite scroll logic part

---
+++ app.js
@@ -0,0 +1,27 @@
+let box =  document.getElementById('box')
+let temp = document.documentElement.scrollHeight
+
+
+
+let x = 1;
+
+const appendDiv =  (limit = 26 + x) =>{
+   
+    while(x <=limit){
+        let p = document.createElement("p");
+        p.innerText=`Masai Student ${x}`;
+        box.append(p);
+        x++
+    }
+}
+
+appendDiv();
+
+
+window.addEventListener("scroll" , ()=> {
+
+ if(window.scrollY + window.innerHeight + 1 >= temp )
+   appendDiv();
+
+
+})
